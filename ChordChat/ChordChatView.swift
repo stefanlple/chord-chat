51676a5e1a8d93933ca616e0ef3066d21d3a649b
Stefan Le <81301569+stefanlple@users.noreply.github.com>
2025-07-22 11:57:31 +0200

    added shortcuts

--- ChordChat/ChordChatView.swift
+++ ChordChat/ChordChatView.swift
@@ -16,37 +16,37 @@ struct ChordChatView: View {
                 ButtonComponent(content: "Undo", icon: "arrowshape.turn.up.backward.circle") {
                     print("Undo")
                     viewModel.undo()
-                }
+                }.keyboardShortcut("z", modifiers: [.command])
 
                 // mutating action
                 ButtonComponent(content: "Redo", icon: "arrowshape.turn.up.forward.circle") {
                     print("Redo")
                     viewModel.redo()
-                }
+                }.keyboardShortcut("z", modifiers: [.command, .shift])
 
                 // non mutating action
                 ButtonComponent(content: "Select All", icon: "doc.on.doc") {
                     print("Select All")
                     viewModel.selectAll()
-                }
+                }.keyboardShortcut("a", modifiers: [.command])
 
                 // non mutating action
                 ButtonComponent(content: "Copy", icon: "clipboard") {
                     print("Copy")
                     viewModel.copy()
-                }
+                }.keyboardShortcut("c", modifiers: [.command])
 
                 // mutating action
                 ButtonComponent(content: "Paste", icon: "doc.on.clipboard") {
                     print("Paste")
                     viewModel.paste()
-                }
+                }.keyboardShortcut("v", modifiers: [.command])
 
                 // mutating action
                 ButtonComponent(content: "Cut", icon: "scissors") {
                     print("Cut")
                     viewModel.cut()
-                }
+                }.keyboardShortcut("x", modifiers: [.command])
             }
 
             ButtonComponent(content: "Send", icon: "paperplane") {
